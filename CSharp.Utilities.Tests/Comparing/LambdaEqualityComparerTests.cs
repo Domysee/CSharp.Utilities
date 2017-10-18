c005ff4d100e0214662717d9f421a0bd3d2a6e77
Dominik Weber <accounts@dominik-weber.name>
2017-10-18 14:58:31 +0200

    Add Tests for LambdaEqualityComparer

---
+++ CSharp.Utilities.Tests/Comparing/LambdaEqualityComparerTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace CSharp.Utilities.Tests.Comparing
+{
+    public class LambdaEqualityComparerTests
+    {
+        [Fact]
+        public void LambdaEqualityComparer_ShouldThrowException_IfEqualsParameterIsNull()
+        {
+            Action act = () =>
+            {
+                new LambdaEqualityComparer<string>(null, s => s.GetHashCode());
+            };
+
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void LambdaEqualityComparer_ShouldThrowException_IfGetHashCodeParameterIsNull()
+        {
+            Action act = () =>
+            {
+                new LambdaEqualityComparer<string>((s1, s2) => s1 == s2, null);
+            };
+
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void LambdaEqualityComparer_ShouldInvokeEqualsParameter_IfEqualsIsCalled()
+        {
+            var called = false;
+            var comparer = new LambdaEqualityComparer<string>((s1, s2) =>
+            {
+                called = true;
+                return s1 == s2;
+            }, s => s.GetHashCode());
+
+            comparer.Equals("s1", "s2");
+
+            called.Should().BeTrue();
+        }
+
+        [Fact]
+        public void LambdaEqualityComparer_ShouldInvokeGetHashCodeParameter_IfGetHashCodeIsCalled()
+        {
+            var called = false;
+            var comparer = new LambdaEqualityComparer<string>((s1, s2) => s1 == s2, s =>
+            {
+                called = true;
+                return s.GetHashCode();
+            });
+
+            comparer.GetHashCode("s");
+
+            called.Should().BeTrue();
+        }
+    }
+}
