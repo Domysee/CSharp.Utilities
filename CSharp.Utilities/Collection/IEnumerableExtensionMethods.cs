51c8efde47b1326d55bde52442ad45d7f49602f2
Dominik Weber <accounts@dominik-weber.name>
2017-10-18 14:38:07 +0200

    Add Add Extension Method for IEnumerable

---
+++ CSharp.Utilities/Collection/IEnumerableExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.Utilities.Collection
+{
+    public static class IEnumerableExtensionMethods
+    {
+        public static IEnumerable<T> Add<T>(this IEnumerable<T> enumerable, T addObject)
+        {
+            return enumerable.Concat(new[] { addObject });
+        }
+    }
+}
