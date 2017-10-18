0cc24c7291c6f7c4e47a51e54c499cff6251ee1b
Dominik Weber <accounts@dominik-weber.name>
2017-10-18 14:38:19 +0200

    Add LambdaEqualityComparer

---
+++ CSharp.Utilities/Comparing/LambdaEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Utilities
+{
+    public class LambdaEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private Func<T, T, bool> equals { get; set; }
+        private Func<T, int> getHashCode { get; set; }
+
+        public LambdaEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            this.equals = equals;
+            this.getHashCode = getHashCode ?? (obj => obj.GetHashCode());
+        }
+
+        public bool Equals(T x, T y) => equals(x, y);
+        public int GetHashCode(T obj) => getHashCode(obj);
+    }
+}
